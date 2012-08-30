193c1269032b40004a2642f1198ea6f33f3896d8
Victor <lesyk.victor@gmail.com>
2012-08-31 00:38:53 +0300

    moved images and made clean now we can start coding

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Sns::Application.routes.draw do
-  resources :images
-
-  get "static_pages/home"
-
-  get "static_pages/help"
+  
+  root :to => 'static_pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,10 +48,6 @@ Sns::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
