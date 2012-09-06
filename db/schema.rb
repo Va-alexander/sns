9a7798c3371fae33ee091a094c6777c6170284b3
Alexander Vasylevskyi <Alexander.Vasylevskyi@gmail.com>
2012-09-06 20:18:54 +0300

    Paperclip added to ProjectImages

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120903104855) do
+ActiveRecord::Schema.define(:version => 20120906171718) do
 
   create_table "project_images", :force => true do |t|
     t.string   "name"
     t.string   "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",               :null => false
+    t.datetime "updated_at",               :null => false
+    t.string   "event_image_file_name"
+    t.string   "event_image_content_type"
+    t.integer  "event_image_file_size"
+    t.datetime "event_image_updated_at"
   end
 
 end
